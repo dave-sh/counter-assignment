cfe8a3fbe3639e53da4f9ead12e8964b3fc7b503
David H <davhuang123@gmail.com>
2023-11-19 19:04:16 -0500

    tests

--- src/tests/Counter.test.js
+++ src/tests/Counter.test.js
@@ -1,22 +1,41 @@
+/* eslint-disable testing-library/no-render-in-setup */
 // import necessary react testing library helpers here
 // import the Counter component here
+import {fireEvent, render, screen } from '@testing-library/react';
+import Counter from '../components/Counter.js';
 
 beforeEach(() => {
   // Render the Counter component here
+  render( <Counter> </Counter> );
 })
 
 test('renders counter message', () => {
   // Complete the unit test below based on the objective in the line above
+  const e = screen.getByTestId('count');
+  expect(e).toBeInTheDocument();
 });
 
 test('should render initial count with value of 0', () => {
   // Complete the unit test below based on the objective in the line above
+  const initial = screen.getByTestId('count');
+  expect(initial).toHaveTextContent('0');
 });
 
 test('clicking + increments the count', () => {
-  // Complete the unit test below based on the objective in the line above
+  const increment = screen.getByText('+');
+  const counter = screen.getByTestId('count');
+
+  fireEvent.click(increment);
+
+  expect(counter).toHaveTextContent('1');
 });
 
 test('clicking - decrements the count', () => {
   // Complete the unit test below based on the objective in the line above
+  const decrement = screen.getByText('-');
+  const counter = screen.getByTestId('count');
+
+  fireEvent.click(decrement);
+
+  expect(counter).toHaveTextContent('-1');
 });
